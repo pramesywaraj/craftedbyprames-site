60d5387e6683cb62613a64cb375d3a6c6a46663b
pramesywaraj <jembarpanalar21@gmail.com>
2025-10-18 16:41:40 +0700

    feat: add Hero and Tools sections in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Navbar from "@/components/layout/Navbar";
+import HeroSection from "@/components/sections/Hero";
+import ToolsSection from "@/components/sections/Tools";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <main className="flex flex-col gap-8">
+        <HeroSection />
+        <ToolsSection />
+      </main>
     </>
   );
 }
